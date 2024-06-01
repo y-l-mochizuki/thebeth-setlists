cdde2c28b0a1cf03ed324201c948c692744074d7
y-l-mochizuki <mczk9402@gmail.com>
2024-06-02 00:32:45 +0900

    fix: background height

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
 
 const Background = () => (
   <div
-    className="fixed w-full h-screen top-0 left-0 z-[-1]"
+    className="fixed w-full min-h-screen top-0 left-0 z-[-1]"
     style={{
       background:
         "radial-gradient(circle at top left, rgba(255, 0, 0, 0.2), transparent 50%), radial-gradient(circle at top right, rgba(128, 0, 128, 0.2), transparent 50%), radial-gradient(circle at bottom right, rgba(255, 165, 0, 0.2), transparent 50%), radial-gradient(circle at bottom left, rgba(255, 192, 203, 0.2), transparent 50%)",
