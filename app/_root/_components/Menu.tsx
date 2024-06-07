0b01d28e3e4c82d5373a6b115b3f9de46205d8b4
y-l-mochizuki <mczk9402@gmail.com>
2024-06-07 23:42:47 +0900

    fix: scroll

--- app/_root/_components/Menu.tsx
+++ app/_root/_components/Menu.tsx
@@ -16,7 +16,7 @@ export const Menu = () => {
   const handleClick = (url: string) => {
     onClose();
     setTimeout(() => {
-      router.push(url);
+      router.push(url, { scroll: false });
     }, 500);
   };
 
