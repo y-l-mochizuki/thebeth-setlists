abf32477c2c0a31d6fa2ace5280eff8c5c34c7ee
y-l-mochizuki <mczk9402@gmail.com>
2024-06-11 21:18:29 +0900

    feature: setlist category

--- app/setlists/page.tsx
+++ app/setlists/page.tsx
@@ -1,19 +1,41 @@
+import { Setlist, getTheBethSetlists } from "@/utils/api";
 import { Card } from "@/components";
-import { getTheBethSetlists } from "@/utils/api";
+import { CategoryTabs } from "./CategoryTabs";
 
 export default async function Home() {
   const setlists = await getTheBethSetlists();
+  const filteredTheBethSetlists = setlists.filter(
+    (setlist) => setlist.category?.thebest_thebeth,
+  );
+  const filteredTaibanSetlists = setlists.filter(
+    (setlist) => setlist.category?.taiban,
+  );
+
+  return (
+    <div className="grid gap-4">
+      <CategoryTabs
+        all={<Tab setlists={setlists} />}
+        thebest_thebeth={<Tab setlists={filteredTheBethSetlists} />}
+        taiban={<Tab setlists={filteredTaibanSetlists} />}
+      />
+    </div>
+  );
+}
+
+type TabProps = {
+  setlists: Setlist[];
+};
+const Tab = ({ setlists }: TabProps) => {
   return (
     <Card.List>
       {setlists.map((setlist) => (
         <Card
+          {...setlist}
           href={`/setlists/${setlist.id}`}
-          image={setlist.image}
-          title={setlist.title}
           date={setlist.live_date}
           key={setlist.id}
         />
       ))}
     </Card.List>
   );
-}
+};
